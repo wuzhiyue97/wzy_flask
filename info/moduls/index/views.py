225bbb8a68e09d197f415f2b02b6f47253bdaa49
wuzhiyue97 <838142354@qq.com>
2020-05-07 23:14:32 +0800

    添加静态文件,模板文件夹

--- info/moduls/index/views.py
+++ info/moduls/index/views.py
@@ -3,5 +3,5 @@ from info import redis_store
 # 3.使用蓝图对象装饰视图函数
 @ index_bp.route('/')
 def index():
-    redis_store.setex('name','laowang')
+    redis_store.setex('name',30,'laowang')
     return 'index'
